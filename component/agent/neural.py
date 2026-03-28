e949f78da0050bacb8451a97f109234c9d3bbcd3
hydroshiba <phanhaiminh.hcm@gmail.com>
2026-03-28 10:52:30 +0000

    Update: Fix proper call to use CPU device for Neural agent

--- component/agent/neural.py
+++ component/agent/neural.py
@@ -6,7 +6,8 @@ class Neural:
 		self.model.eval()
 	
 	def play(self, board, player):
-		move_values = self.model.policy(board.board * player)
+		device = next(self.model.parameters()).device
+		move_values = self.model.policy((board.board * player).to(device)).cpu()
 		legal_moves = board.legal_moves()
 		best_move = [move for move in legal_moves if move_values[move] == move_values[legal_moves].max()][0]
 		return best_move
